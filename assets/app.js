10934db9785b4dfab1955a08c2fe3ff182ffab4a
damienlebon <damienlebon.ifpa@gmail.com>
2024-07-23 12:34:08 +0200

    setup: home presentation

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,6 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.sass';
+import './js/main.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');

550ae94fb1319d1cc4f8a5371e693a1df82b3573
damienlebon <damienlebon.ifpa@gmail.com>
2024-07-25 14:49:50 +0200

    style: portfolio page

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class PortfolioController extends AbstractController
 {
-    #[Route('/portfolio', name: 'app_portfolio')]
+    #[Route('/portfolio', name: 'portfolio')]
     public function index(): Response
     {
         return $this->render('portfolio/index.html.twig', [

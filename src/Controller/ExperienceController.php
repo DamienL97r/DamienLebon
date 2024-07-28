a2b0f4f94c1cddfdad37805d2f34b6fa7448736e
damienlebon <damienlebon.ifpa@gmail.com>
2024-07-28 23:59:46 +0200

    display: exp on experiences page

--- src/Controller/ExperienceController.php
+++ src/Controller/ExperienceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ExperienceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class ExperienceController extends AbstractController
 {
     #[Route('/mes-experiences', name: 'experience')]
-    public function index(): Response
+    public function index(
+        ExperienceRepository $experienceRepository
+    ): Response
     {
         return $this->render('experience/index.html.twig', [
-            'controller_name' => 'ExperienceController',
+            'experiences' => $experienceRepository->findAll(),
         ]);
     }
 }

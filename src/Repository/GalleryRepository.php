ce7437fb504fbb4b37da59dd8fc7f7192ac2914c
damienlebon <damienlebon.ifpa@gmail.com>
2024-07-30 14:58:06 +0200

    display: image gallery on about page

--- src/Repository/GalleryRepository.php
+++ src/Repository/GalleryRepository.php
@@ -16,28 +16,12 @@ class GalleryRepository extends ServiceEntityRepository
         parent::__construct($registry, Gallery::class);
     }
 
-    //    /**
-    //     * @return Gallery[] Returns an array of Gallery objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('g')
-    //            ->andWhere('g.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('g.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Gallery
-    //    {
-    //        return $this->createQueryBuilder('g')
-    //            ->andWhere('g.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function getAllLatest(): array
+    {
+        return $this->createQueryBuilder('g')
+            ->orderBy('g.id', 'DESC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
